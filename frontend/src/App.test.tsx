cc2175dda7029cc450fd314a12fefc720876ff9a
Olli Tanhuanpää <ottanh@utu.fi>
2022-07-29 08:46:43 +0300

    initial commit

---
+++ frontend/src/App.test.tsx
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders hello world', () => {
+  render(<App />);
+  const h = screen.getByText('Hello world');
+  expect(h).toBeInTheDocument();
+});
