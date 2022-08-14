1291490b03b66cd0d11f9e2f0cc3f005172896f6
Olli Tanhuanpää <ottanh@utu.fi>
2022-08-14 13:41:54 +0300

    fix unused env variable error

--- backend/src/util/config.ts
+++ backend/src/util/config.ts
@@ -5,9 +5,5 @@ if(!process.env.DATABASE_URL){
   throw new Error('DATABASE_URL is undefined');
 }
 
-if(!process.env.SECRET){
-  throw new Error('SECRET is undefined');
-}
-
 export const DATABASE_URL = process.env.DATABASE_URL;
 export const PORT = process.env.PORT || 3001;
