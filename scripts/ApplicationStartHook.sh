cd49c53cc7edcbee70ec1f006d5f6a73cb6a3ea6
Olli Tanhuanpää <ottanh@utu.fi>
2022-08-14 11:57:17 +0300

    fix build path in ApplicationStartHook.sh

--- scripts/ApplicationStartHook.sh
+++ scripts/ApplicationStartHook.sh
@@ -1,6 +1,6 @@
 source /home/ec2-user/.bash_profile
 export NVM_DIR="/home/ec2-user/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
-cd /bike-app
+cd /bike-app/backend
 pm2 delete all
 PORT=80 pm2 start build/index.js 
