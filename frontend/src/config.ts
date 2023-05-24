afcd0990e3490cf4410b903fabe14893d89387ac
Olli Tanhuanpää <84331406+Ottanh@users.noreply.github.com>
2023-05-24 04:31:14 +0300

    update backend url

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,3 +1,3 @@
 export const BACKEND_URI = process.env.NODE_ENV === 'production' 
-  ? 'http://ec2-16-171-62-148.eu-north-1.compute.amazonaws.com' 
+  ? 'http://ec2-16-16-213-173.eu-north-1.compute.amazonaws.com/' 
   : 'http://localhost:3001';
